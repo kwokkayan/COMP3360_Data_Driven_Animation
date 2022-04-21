502cf414b6cc25a264e8ace9ea27d788d5454c16
Kwok Ka Yan <kky0712@connect.hku.hk>
2022-04-22 01:11:16 +0800

    fin ass 3

---
+++ pba_tutorial3_fem/CMakeFiles/fem-tutorial.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem/glad.c" "/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem/CMakeFiles/fem-tutorial.dir/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "PROJECT_NAME_STR=\"fem-tutorial\""
+  "PROJECT_ROOT_DIR=\"/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "extern/glm"
+  "extern"
+  "extern/glfw/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem/gl_utils.cpp" "/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem/CMakeFiles/fem-tutorial.dir/gl_utils.cpp.o"
+  "/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem/main.cpp" "/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem/CMakeFiles/fem-tutorial.dir/main.cpp.o"
+  "/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem/trimesh.cpp" "/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem/CMakeFiles/fem-tutorial.dir/trimesh.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "PROJECT_NAME_STR=\"fem-tutorial\""
+  "PROJECT_ROOT_DIR=\"/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "extern/glm"
+  "extern"
+  "extern/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kwokkayan/COMP3360_Data_Driven_Animation/pba_tutorial3_fem/extern/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pba_tutorial3_fem/CMakeFiles/fem-tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fem-tutorial.dir/gl_utils.cpp.o"
+  "CMakeFiles/fem-tutorial.dir/glad.c.o"
+  "CMakeFiles/fem-tutorial.dir/main.cpp.o"
+  "CMakeFiles/fem-tutorial.dir/trimesh.cpp.o"
+  "fem-tutorial"
+  "fem-tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/fem-tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
